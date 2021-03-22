c35309ab12c8a1705a8f98e7562cfe9f05c31b3d
Thomas Ruggeri <truggeri@gmail.com>
2021-03-21 20:22:32 -0400

    Lighthouse imrpovements

--- functions/middleware.go
+++ functions/middleware.go
@@ -14,7 +14,7 @@ func OnlyGet(w http.ResponseWriter, r *http.Request, h Handler) {
 		return
 	}
 
-	h(w, r)
+	addSecurityHeaders(w, r, h)
 }
 
 // OnlyPost Blocks all requests except POSTs
@@ -24,5 +24,11 @@ func OnlyPost(w http.ResponseWriter, r *http.Request, h Handler) {
 		return
 	}
 
+	addSecurityHeaders(w, r, h)
+}
+
+func addSecurityHeaders(w http.ResponseWriter, r *http.Request, h Handler) {
+	w.Header().Add("X-XSS-Protection", "1; mode=block")
+	w.Header().Add("Content-Security-Policy", "default-src 'self'; font-src 'self'; frame-src 'none'; img-src 'self'; media-src 'none'; object-src 'none'; script-src 'self'; style-src 'self'")
 	h(w, r)
 }
