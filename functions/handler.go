1b24b3619e2bef562c252eef7fa523ab382f3c81
Thomas Ruggeri <truggeri@gmail.com>
2021-05-13 09:59:26 -0400

    Switching param parsing techniques

--- functions/handler.go
+++ functions/handler.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-// Authorize prints only on GET request
+// Authorize Displays user form to log in and authorize the requesting client
 func Authorize(w http.ResponseWriter, r *http.Request) {
 	mw := []Middleware{OnlyAllow(http.MethodGet), SetCsrfCookie(), ParamsFromQuery()}
 	handler := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
@@ -15,8 +15,9 @@ func Authorize(w http.ResponseWriter, r *http.Request) {
 	wrapMiddleware(mw, handler)(r.Context(), w, r)
 }
 
+// CodeGrant Creates a new user and returns the redirect associated with the client
 func CodeGrant(w http.ResponseWriter, r *http.Request) {
-	mw := []Middleware{OnlyAllow(http.MethodPost), ParamsFromJson(), ValidateCsrfToken()}
+	mw := []Middleware{OnlyAllow(http.MethodPost), ParamsFromBody(), ValidateCsrfToken()}
 	handler := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 		codeGrant(ctx, w, r)
 		return nil
@@ -24,15 +25,24 @@ func CodeGrant(w http.ResponseWriter, r *http.Request) {
 	wrapMiddleware(mw, handler)(r.Context(), w, r)
 }
 
-// CreateClient generates and returns client codes
+// CreateClient Creates a new client to be used with the app
 func CreateClient(w http.ResponseWriter, r *http.Request) {
-	mw := []Middleware{OnlyAllow(http.MethodPost), ParamsFromBody()}
+	mw := []Middleware{OnlyAllow(http.MethodPost), ParamsFromJson()}
 	handler := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 		createClient(ctx, w, r)
 		return nil
 	}
 	wrapMiddleware(mw, handler)(r.Context(), w, r)
 }
+
+// func Info(w http.ResponseWriter, r *http.Request) {
+// 	mw := []Middleware{OnlyAllow(http.MethodGet), ValidateAuth()}
+// 	handler := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+// 		info(ctx, w, r)
+// 		return nil
+// 	}
+// 	wrapMiddleware(mw, handler)(r.Context(), w, r)
+// }
 
 // Token Returns authorization token and user info
 func Token(w http.ResponseWriter, r *http.Request) {
